ad45b013c34b3ab9846d7ca7dbd89d914823754e
hussain <hussainhanif1612@gmail.com>
2023-05-10 00:12:29 +0500

    faq amd images

--- resources/views/partials/faq.blade.php
+++ resources/views/partials/faq.blade.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div id="faq" class="container">
     <!--Section: FAQ-->
     <section class="faq">
         <h2 class="text-center mb-2 pb-2 text-primary-app fw-bold">Best Youtube to MP4 Downloader
@@ -7,7 +7,9 @@
             <div class="custom_container">
                 <div class="row">
                     <div class="col-md-12 text-center">
-                        <p class="intro-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsam quaerat nihil, qui praesentium, aspernatur deleniti culpa error reiciendis aperiam soluta quia quasi magnam sit? Culpa, nihil quaerat! Nam, maiores?</p>
+                        <p class="intro-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsam quaerat
+                            nihil, qui praesentium, aspernatur deleniti culpa error reiciendis aperiam soluta quia quasi
+                            magnam sit? Culpa, nihil quaerat! Nam, maiores?</p>
                     </div>
                 </div>
                 <br>
@@ -15,11 +17,18 @@
                 <br>
                 <div class="row">
                     <div class="col-md-7">
-                        <h3 class="text-primary-app mb-2 pb-2"><b>How to Download Youtube Video 4reedownloadonline?</b></h3>
+                        <h3 class="text-primary-app mb-2 pb-2"><b>How to Download Youtube Video 4reedownloadonline?</b>
+                        </h3>
                         <div class="features">
                             <ul class="itemstyle">
-                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum suscipit, sed repellendus commodi hic quidem assumenda laudantium quisquam! Doloremque obcaecati incidunt nihil corrupti quos voluptate eaque deserunt inventore perferendis qui!</li>
-                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum suscipit, sed repellendus commodi hic quidem assumenda laudantium quisquam! Doloremque obcaecati incidunt nihil corrupti quos voluptate eaque deserunt inventore perferendis qui!</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum suscipit, sed
+                                    repellendus commodi hic quidem assumenda laudantium quisquam! Doloremque obcaecati
+                                    incidunt nihil corrupti quos voluptate eaque deserunt inventore perferendis qui!
+                                </li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum suscipit, sed
+                                    repellendus commodi hic quidem assumenda laudantium quisquam! Doloremque obcaecati
+                                    incidunt nihil corrupti quos voluptate eaque deserunt inventore perferendis qui!
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -27,7 +36,9 @@
                         <h3 class="text-primary-app mb-2 pb-2"><b>Lorem ipsum dolor sit </b></h3>
                         <div class="features">
                             <ul class="itemstyle">
-                               <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odit fuga ipsam tempora maiores, facilis numquam unde harum totam natus. Eius alias minima ipsam nesciunt dolore maxime accusantium ea vitae!</li>
+                                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque odit fuga ipsam
+                                    tempora maiores, facilis numquam unde harum totam natus. Eius alias minima ipsam
+                                    nesciunt dolore maxime accusantium ea vitae!</li>
                             </ul>
                         </div>
                     </div>
@@ -36,65 +47,39 @@
                 <hr>
                 <br>
                 <div class="row">
-                    <div class="col-md-6 col-lg-4 mb-4">
-                        <h5 class="mb-3 text-primary-app"><i class="far fa-paper-plane text-primary-app pe-2"></i> A
-                            simple
-                            question?</h5>
-                        <p>
-                            <strong><u>Absolutely!</u></strong> We work with top payment companies which guarantees
-                            your
-                            safety and
-                            security. All billing information is stored on our payment processing partner.
-                        </p>
-                    </div>
-
-                    <div class="col-md-6 col-lg-4 mb-4">
-                        <h5 class="mb-3 text-primary-app"><i class="fas fa-pen-alt text-primary-app pe-2"></i> A
-                            question
-                            that
-                            is longer then the previous one?</h5>
-                        <p>
-                            <strong><u>Yes, it is possible!</u></strong> You can cancel your subscription anytime in
-                            your
-                            account. Once the subscription is
-                            cancelled, you will not be charged next month.
-                        </p>
-                    </div>
-
-                    <div class="col-md-6 col-lg-4 mb-4">
-                        <h5 class="mb-3 text-primary-app"><i class="fas fa-user text-primary-app pe-2"></i> A simple
-                            question?
-                        </h5>
-                        <p>
-                            Currently, we only offer monthly subscription. You can upgrade or cancel your monthly
-                            account at any time with no further obligation.
-                        </p>
-                    </div>
-
-                    <div class="col-md-6 col-lg-4 mb-4">
-                        <h5 class="mb-3 text-primary-app"><i class="fas fa-rocket text-primary-app pe-2"></i> A simple
-                            question?
-                        </h5>
-                        <p>
-                            Yes. Go to the billing section of your dashboard and update your payment information.
-                        </p>
-                    </div>
-
-                    <div class="col-md-6 col-lg-4 mb-4">
-                        <h5 class="mb-3 text-primary-app"><i class="fas fa-home text-primary-app pe-2"></i> A simple
-                            question?
-                        </h5>
-                        <p><strong><u>Unfortunately no</u>.</strong> We do not issue full or partial refunds for any
-                            reason.</p>
-                    </div>
-
-                    <div class="col-md-6 col-lg-4 mb-4">
-                        <h5 class="mb-3 text-primary-app"><i class="fas fa-book-open text-primary pe-2"></i> Another
-                            question that is longer than usual</h5>
-                        <p>
-                            Of course! We’re happy to offer a free plan to anyone who wants to try our service.
-                        </p>
-                    </div>
+                    <div class="row pt-5">
+                        <div class="col-md-4 text-center d-flex flex-column justify-content-center">
+                          <span class="glyphicon40 glyphicon glyphicon-dashboard"><img src="{{ asset('assets/images/conversion.svg') }}" class="img-thumbnail" alt="faster conversion"></span>
+                          <h4><b>Unlimited Conversion</b></h4>
+                          <p>We use the best Servers for the conversion of your Youtube Video to MP4, moreover, the encoding will not reduce the video quality.</p>
+                        </div>
+                        <div class="col-md-4 text-center d-flex flex-column justify-content-center">
+                          <span class="glyphicon40 glyphicon glyphicon-check"><img src="{{ asset('assets/images/download.svg') }}" class="img-thumbnail" alt=""></span>
+                          <h4><b>Add Fetch Youtube</b></h4>
+                          <p>Our interface is simple and easy to use, You just have to paste the video link (URL) of a Youtube video and we will do the rest.</p>
+                        </div>
+                        <div class="col-md-4 text-center d-flex flex-column justify-content-center">
+                          <span class="glyphicon40 glyphicon glyphicon-gift"><img src="{{ asset('assets/images/registeration.svg') }}" class="img-thumbnail" alt=""></span>
+                          <h4><b>No registration required</b></h4>
+                          <p>Our Youtube to mp4 converter and downloader is a free service, also there are no restrictions about the number of files you want to convert.</p>
+                        </div>
+                        <div class="col-md-4 text-center d-flex flex-column justify-content-center">
+                          <span class="glyphicon40 second-line glyphicon glyphicon-minus-sign"><img src="{{ asset('assets/images/fast.svg') }}" class="img-thumbnail" alt=""></span>
+                          <h4><b>Faster</b></h4>
+                          <p>YouTube offers a premium subscription service to download videos for offline viewing. However, with Y2Mate, you can download your favorite videos to mp4 without signing in or having a subscription.</p>
+                        </div>
+                        <div class="col-md-4 text-center d-flex flex-column justify-content-center">
+                          <span class="glyphicon40 second-line glyphicon glyphicon-hd-video"><img src="{{ asset('assets/images/compability.svg') }}" class="img-thumbnail" alt=""></span>
+                          <h4><b>Browser Compatibility</b></h4>
+                          <p>YouTube reduces the video quality to stream without interruption. With Y2Mate, you can save videos in their original HD format without losing quality.</p>
+                        </div>
+                        <div class="col-md-4 text-center d-flex flex-column justify-content-center">
+                          <span class="glyphicon40 second-line glyphicon glyphicon-play"><img src="{{ asset('assets/images/friendly.svg') }}" class="img-thumbnail" alt=""></span>
+                          <h4><b>Mobile Friendly</b></h4>
+                          <p>Download YouTube videos and save them in Mp4 format to watch anytime, anywhere, without using your internet data. It’s also useful when traveling in areas without internet connectivity.</p>
+                        </div>
+                      </div>
+                      
                 </div>
             </div>
         </section>
--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,4 +1,4 @@
-<footer>
+{{-- <footer>
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
@@ -8,5 +8,118 @@
         </p>
         </div>
       </div>
+       
     </div>
-  </footer>
+  </footer> --}}
+
+<!-- Footer -->
+<footer class="bg-primary text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4">
+        <!-- Section: Text -->
+        <section class="mb-4">
+            <p>
+              {{ __('footer.sub_heading_1') }}
+            </p>
+        </section>
+        <!-- Section: Text -->
+        <!-- Section: Links -->
+     <section class="link-list">
+    <!--Grid row-->
+    {{-- <div class="row d-flex justify-content-center">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#faq" class="text-white">FAQ</a>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#" class="text-white">Terms of Service</a>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#faq" class="text-white">Privacy Policy</a>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#faq" class="text-white">FAQ</a>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+    </div> --}}
+    <!--Grid row-->
+    </section>
+
+    <section class="link-list">
+      <div class="container">
+          <div class="row justify-content-center">
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0 d-flex align-items-center justify-content-center">
+                  <ul class="list-unstyled mb-0">
+                      <li>
+                          <a href="#faq" class="text-white">FAQ</a>
+                      </li>
+                  </ul>
+              </div>
+  
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0 d-flex align-items-center justify-content-center">
+                  <ul class="list-unstyled mb-0">
+                      <li>
+                          <a href="#" class="text-white">Terms of Service</a>
+                      </li>
+                  </ul>
+              </div>
+  
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0 d-flex align-items-center justify-content-center">
+                  <ul class="list-unstyled mb-0">
+                      <li>
+                          <a href="#faq" class="text-white">Privacy Policy</a>
+                      </li>
+                  </ul>
+              </div>
+  
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0 d-flex align-items-center justify-content-center">
+                  <ul class="list-unstyled mb-0">
+                      <li>
+                          <a href="#faq" class="text-white">FAQ</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+  </section>
+  
+
+        <!-- Section: Links -->
+
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      {{ __('footer.copyright') }} :
+        <a class="text-white" href="/">4reedownloadonline.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
